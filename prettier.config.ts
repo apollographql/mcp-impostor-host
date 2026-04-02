ffaf10fd6b725706486d5a0908fec359b9d4e2a0
Jerel Miller <jerelmiller@gmail.com>
2026-04-01 18:15:11 -0600

    Add prettier

---
+++ prettier.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "es5",
+  semi: true,
+  printWidth: 80,
+  experimentalTernaries: true,
+};
+
+export default config;
