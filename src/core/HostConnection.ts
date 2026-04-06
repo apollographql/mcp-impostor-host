e7fe37534d6637c517dfd031d4ad398a8c7800ea
Jerel Miller <jerelmiller@gmail.com>
2026-04-06 15:44:25 -0600

    Add findTool method

--- src/core/HostConnection.ts
+++ src/core/HostConnection.ts
@@ -1,9 +1,38 @@
 import type { Client } from "@modelcontextprotocol/sdk/client";
+import type { Tool } from "@modelcontextprotocol/sdk/types";
 
 export class HostConnection {
   private client: Client;
+  private toolInfoByName = new Map<string, Tool>();
 
   constructor(client: Client) {
     this.client = client;
+  }
+
+  private nextToolCursor: string | undefined;
+
+  private async findTool(name: string): Promise<Tool | undefined> {
+    let tool: Tool | undefined = this.toolInfoByName.get(name);
+
+    if (tool) {
+      return tool;
+    }
+
+    const result = await this.client.listTools({ cursor: this.nextToolCursor });
+    this.nextToolCursor = result.nextCursor;
+
+    for (const toolDef of result.tools) {
+      this.toolInfoByName.set(toolDef.name, toolDef);
+
+      if (toolDef.name === name) {
+        tool = toolDef;
+      }
+    }
+
+    if (!tool && this.nextToolCursor) {
+      tool = await this.findTool(name);
+    }
+
+    return tool;
   }
 }
