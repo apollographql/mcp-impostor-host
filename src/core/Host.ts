09997cc1cb07f19fbfd3814800ef32de436e0f89
Jerel Miller <jerelmiller@gmail.com>
2026-04-06 14:39:36 -0600

    Stub more of the host

--- src/core/Host.ts
+++ src/core/Host.ts
@@ -1,5 +1,11 @@
+import { Client } from "@modelcontextprotocol/sdk/client";
+import { UI_EXTENSION_CAPABILITIES } from "@mcp-ui/client";
+import pkg from "#package.json" with { type: "json " };
+import { HostConnection } from "./HostConnection";
+import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp";
+
 interface SandboxConfig {
-  uri: string;
+  url: string;
 }
 
 interface HostConfig {
@@ -7,15 +13,28 @@ interface HostConfig {
 }
 
 interface ConnectOptions {
-  uri: string;
+  url: string;
 }
 
 export class Host {
   private config: HostConfig;
+  private client: Client;
 
   constructor(config: HostConfig) {
     this.config = config;
+    this.client = new Client(
+      {
+        name: "@apollo/mcp-impostor-host",
+        version: pkg.version,
+      },
+      { capabilities: { extensions: UI_EXTENSION_CAPABILITIES } },
+    );
   }
 
-  async connect(options: ConnectOptions) {}
+  async connect(options: ConnectOptions) {
+    const transport = new StreamableHTTPClientTransport(new URL(options.url));
+    await this.client.connect(transport);
+
+    return new HostConnection(this.client);
+  }
 }
