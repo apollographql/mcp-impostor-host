5bbc9f7f4ff59d188a46ea04cf28e36c42608077
Jerel Miller <jerelmiller@gmail.com>
2026-04-07 12:45:44 -0600

    Use vite to build sandbox html

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: resolve(import.meta.dirname, "dist"),
+    // Don't clean files built from `tsc`.
+    emptyOutDir: false,
+    rolldownOptions: {
+      input: {
+        sandbox: resolve(import.meta.dirname, "src/sandbox/index.html"),
+      },
+    },
+  },
+  plugins: [viteSingleFile()],
+});
