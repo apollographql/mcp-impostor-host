7465971c41a011465712d1632f038b354150eda8
Jerel Miller <jerelmiller@gmail.com>
2026-04-07 19:09:29 -0600

    Automatically set prefix in invariant

--- src/utilities/invariant.ts
+++ src/utilities/invariant.ts
@@ -1,5 +1,5 @@
 export function invariant(condition: any, message: string): asserts condition {
   if (!condition) {
-    throw new Error(message);
+    throw new Error(`[@apollo/mcp-impostor-host] ${message}`);
   }
 }
