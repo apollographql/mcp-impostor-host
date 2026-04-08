466dc0e4b32c4e625969a6aa90374752b1acd4ac
Jerel Miller <jerelmiller@gmail.com>
2026-04-08 15:03:41 -0600

    Add some default styles

--- src/react/Sandbox.tsx
+++ src/react/Sandbox.tsx
@@ -118,7 +118,12 @@ export function Sandbox({ url, connection, execution }: Sandbox.Props) {
     <iframe
       ref={refCallback}
       sandbox="allow-scripts allow-same-origin allow-forms"
-      style={{ border: "none", width: "100%", height: "100%" }}
+      style={{
+        border: "none",
+        width: "100dvw",
+        height: "100dvh",
+        backgroundColor: "transparent",
+      }}
     />
   ) : null;
 }
