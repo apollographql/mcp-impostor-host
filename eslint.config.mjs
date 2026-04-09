a536c7a22e0f8cb997bded5dcd5ccb120dd6fbd3
Jerel Miller <jerelmiller@gmail.com>
2026-04-09 11:13:03 -0600

    Enforce extensions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginImport from "eslint-plugin-import";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
@@ -13,9 +14,13 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReactHooks.configs.flat.recommended,
+  pluginImport.flatConfigs.recommended,
   {
     rules: {
       "@typescript-eslint/no-namespace": "off",
+      "import/extensions": ["error", "always", { ignorePackages: true }],
+      // This rule has too many false positives
+      "import/no-unresolved": "off",
     },
   },
 ]);
